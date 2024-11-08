eae891cd725b443ea102703ddaef36828ebc1cff
cesium132dot9 <sunc20060429@gmail.com>
2024-11-07 23:32:51 -0500

    too aggresive change

--- girlfriend/app/yes/page.js
+++ girlfriend/app/yes/page.js
@@ -4,7 +4,7 @@ export default function Yes() {
         <div className="h-screen w-screen flex bg-red justify-center items-center">
             <div className="text-center">
                 <h1 className="text-8xl p-2 m-4">YEA!!</h1>
-                <h2 className="text-3xl">Make out? (Say yes)</h2>
+                <h2 className="text-3xl">Make out?</h2>
                 <iframe src="https://giphy.com/embed/26FLdmIp6wJr91JAI" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> 
             </div>
         </div>
